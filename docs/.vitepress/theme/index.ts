97883170536427ededcd7b1577270b7afcb23eda
ywpc05 <ywpc05@gmail.com>
2026-07-09 23:21:10 +0800

    feat: display tags on blog post pages via doc-before slot; nudge features up

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -6,12 +6,21 @@ import { withBlogTheme } from 'vitepress-plugin-blog'
 import 'vitepress-plugin-blog/style.css'
 import GiscusComment from './components/GiscusComment.vue'
 import Busuanzi from './components/Busuanzi.vue'
+import TagsDisplay from './components/TagsDisplay.vue'
 import './style/vars.css'
 
 const enhanced = {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
+      'doc-before': () => {
+        const { frontmatter } = useData()
+        const tags = frontmatter.value.tags
+        if (tags && Array.isArray(tags) && tags.length) {
+          return h(TagsDisplay, { tags })
+        }
+        return null
+      },
       'doc-after': () => {
         const { frontmatter } = useData()
         const showComments = frontmatter.value.comment !== false
